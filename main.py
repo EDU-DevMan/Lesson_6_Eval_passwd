40f667b0bdd598944d19a00d72d86fba4a4fb839
Ansle <89898234+ivan32rus@users.noreply.github.com>
2024-11-12 00:01:02 +0300

    added password complexity verification code

---
+++ main.py
@@ -0,0 +1,84 @@
+import urwid
+
+
+def is_very_long(password):
+    return len(password) > 12
+
+
+def has_digit(password):
+    return any(symbol.isdigit() for symbol in password)
+
+
+def has_letters(password):
+    return any(symbol.isalpha() for symbol in password)
+
+
+def has_upper_letters(password):
+    return any(symbol.isupper() for symbol in password)
+
+
+def has_lower_letters(password):
+    return (
+        any(symbol.islower() for symbol in password)
+        and any(symbol.isupper() for symbol in password)
+        )
+
+
+def has_symbols(password):
+    return (
+        not password.isalnum()
+        and len(password) > 0
+        and not any(password.isspace() for symbol in password)
+        )
+
+
+def main():
+    scope = 0
+    passwd = input("Введите пароль: ")
+
+    checking_functions = [
+        is_very_long(passwd),
+        has_digit(passwd),
+        has_letters(passwd),
+        has_upper_letters(passwd),
+        has_lower_letters(passwd),
+        has_symbols(passwd),
+    ]
+
+    for verific in checking_functions:
+        if verific:
+            scope = scope + 2
+
+    return scope
+
+
+if __name__ == '__main__':
+    print(main())
+
+
+
+# def on_ask_change(edit, passwd):
+#     scope = 0
+
+#     checking_functions = [
+#         is_very_long(passwd),
+#         has_digit(passwd),
+#         has_letters(passwd),
+#         has_upper_letters(passwd),
+#         has_lower_letters(passwd),
+#         has_symbols(passwd),
+#     ]
+
+#     for verific in checking_functions:
+#         if verific:
+#             scope = scope + 2
+
+#     reply.set_text("Рейтинг пароля: %s" % scope)
+
+
+# passwd = urwid.Edit('Введите пароль: ', mask='*')
+# reply = urwid.Text("")
+# menu = urwid.Pile([passwd, reply])
+# menu = urwid.Filler(menu, valign='top')
+# urwid.connect_signal(passwd, 'change', on_ask_change)
+# urwid.MainLoop(menu).run()
